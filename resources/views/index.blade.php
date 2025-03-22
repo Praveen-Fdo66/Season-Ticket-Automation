1fdfe6530797a576cea80c5fa7524b1a9f3e1340
Praveen Fernando <fernandojr662005@gmail.com>
2025-03-22 23:43:28 +0530

    CHANGE 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
 
     <div class="content">
         <h1>Welcome to<span class="tng">Tap and Go</span></h1>
-        <p>Tap and Go - Smart, fast, and hassle-free seasonal ticketing.<br> Just tap, travel, and enjoy the ride!</p>
+        <p>Tap and Go - CHANGE 1 Smart, fast, and hassle-free seasonal ticketing.<br> Just tap, travel, and enjoy the ride!</p>
         <a href="s_register"><button type="button" class="btn1">Register</button></a>
         <button type="button" class="btn1">Renew Ticket</button>
 
